80c90eea98ae04e84529d4ce807df28dfdc95815
alexandradragomir01 <152998998+alexandradragomir01@users.noreply.github.com>
2025-04-12 21:56:26 +0300

    Update endpoints.js

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,5 +1,5 @@
 // Nu este safe sa pui cheia aici, dar fara un server n-ai cum sa fii safe oricum. Nu iti face griji, API-ul este gratuit, oricum.
-const API_KEY = "1ef6d2fa-4f80-40c5-8d47-7fcbd0028471";
+const API_KEY = "d95f6b00-f76e-4dda-84a8-a0639ee0a08b";
 
 // Functie care returneaza endpoint-ul pentru o anumita categorie de stiri.
 // pageNumber si pageSize au valori default, astfel incat daca nu le specifici o valoare la apel, vor primi automat valorile default.
